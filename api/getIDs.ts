ba1e764873be0f2f43812259056d1bfe02bda1a8
TH3C0D3R <7798500+Th3C0D3R@users.noreply.github.com>
2024-03-27 11:08:40 +0100

    update

--- api/getIDs.ts
+++ api/getIDs.ts
@@ -4,5 +4,5 @@ import { sql } from "@vercel/postgres";
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     const idsRow = await sql`SELECT id FROM ids;`;
     const ids = idsRow.rows.map(x => x.id);
-    return res.json(ids);
+    return res.json({ "ids": ids });
 }
