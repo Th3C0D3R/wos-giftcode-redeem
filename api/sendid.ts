3048d714e98b6b58a026426aab86f85d770eb7b1
TH3C0D3R <7798500+Th3C0D3R@users.noreply.github.com>
2024-03-27 11:02:11 +0100

    fix

--- api/sendid.ts
+++ api/sendid.ts
@@ -6,9 +6,9 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     if (data["id"] === undefined) return res.json({ status: 500, data: "NO PLAYER ID" });
     var idToAdd = data["id"] as string;
     const idsRow = await sql`SELECT id FROM ids WHERE id = ${idToAdd};`;
-    if(idsRow.rowCount > 0) return res.json({ status: 200, data: `ID ${idToAdd} already added` });
+    if(idsRow.rowCount > 0) return res.json({ status: 302, data: `ID ${idToAdd} already added` });
 
-    var result = await sql`INSERT INTO ids VALUES (${idToAdd}); `
+    var result = await sql`INSERT INTO ids VALUES (${idToAdd}); `;
     if(result.rowCount >= 0) return res.json({ status: 200, data: `ID ${idToAdd} added` });
     return res.json({ status: 500, data: `SQL failed to add ID ${idToAdd}` });
 }
