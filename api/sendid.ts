026e9b0102f1029e094c9b69ead8b8504738f603
TH3C0D3R <7798500+Th3C0D3R@users.noreply.github.com>
2024-03-27 10:53:17 +0100

    update

--- api/sendid.ts
+++ api/sendid.ts
@@ -4,18 +4,11 @@ import { sql } from "@vercel/postgres";
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     var data = req.query;
     if (data["id"] === undefined) return res.json({ status: 500, data: "NO PLAYER ID" });
+    var idToAdd = data["id"] as string;
+    const idsRow = await sql`SELECT id FROM ids WHERE id = ${idToAdd};`;
+    if(idsRow.rowCount > 0) return res.json({ status: 200, data: `ID ${idToAdd} already added` });
 
-    var ids = await sql`SELECT * FROM ids`;
-    console.log(ids.rows);
-    console.log(ids.rows.values());
-    return res.json({ status: 200, data: "IDs added" });
-    ids["ids"] = ids["ids"].concat([data["id"]]);
-    ids["ids"].sort(function (a: number, b: number) {
-        return a - b;
-    });
-
-    ids["ids"] = [...new Set(ids["ids"])];
-
-
-    return res.json({ status: 200, data: "IDs added" });
+    var result = await sql`INSERT INTO ids VALUES (${idToAdd}); `
+    if(result.rowCount >= 0) return res.json({ status: 200, data: `ID ${idToAdd} added` });
+    return res.json({ status: 500, data: `SQL failed to add ID ${idToAdd}` });
 }
