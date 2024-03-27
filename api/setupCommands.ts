7b7f401c6fc1bcf7c74035fa3fe5df0ac5969af4
TH3C0D3R <7798500+Th3C0D3R@users.noreply.github.com>
2024-03-27 14:46:29 +0100

    update

--- api/setupCommands.ts
+++ api/setupCommands.ts
@@ -7,44 +7,53 @@ const discordHeaders = {
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     const url = `https://discord.com/api/v10/applications/${process.env.PRIVATE_DISCORD_ID}/commands`;
-    var data = [] as Array<any>;
-    data = [
-        {
-            name: "addplayer",
-            type: 1,
-            description: 'Add Player(s) to the Redeem-List',
-            options: [
-                {
-                    "name": "ids",
-                    "description": "All PlayerIDs you want to add separated by a ';'",
-                    "type": 3,
-                    "required": true,
-                    "max_length": 6000
-                }
-            ]
-        },
-        {
-            name: "playerinfo",
-            type: 1,
-            description: 'Get all possible data from one player',
-            options: [
-                {
-                    "name": "id",
-                    "description": "the player-id you want the info",
-                    "type": 4,
-                    "required": true
-                }
-            ]
-        },
-    ];
+    var addPlayerPayload =
+    {
+        name: "addplayer",
+        type: 1,
+        description: 'Add Player(s) to the Redeem-List',
+        options: [
+            {
+                "name": "ids",
+                "description": "All PlayerIDs you want to add separated by a ';'",
+                "type": 3,
+                "required": true,
+                "max_length": 6000
+            }
+        ]
+    };
+    var getPlayerInfoPayload = {
+        name: "playerinfo",
+        type: 1,
+        description: 'Get all possible data from one player',
+        options: [
+            {
+                "name": "id",
+                "description": "the player-id you want the info",
+                "type": 4,
+                "required": true
+            }
+        ]
+    }
 
-    const response = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": discordHeaders.Authorization
-        },
-        body: JSON.stringify(data), // body data type must match "Content-Type" header
-    });
-    return res.json(await response.json());
+    var results = await Promise.all(
+        [
+            fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": discordHeaders.Authorization
+                },
+                body: JSON.stringify(addPlayerPayload), // body data type must match "Content-Type" header
+            }),
+            fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": discordHeaders.Authorization
+                },
+                body: JSON.stringify(getPlayerInfoPayload), // body data type must match "Content-Type" header
+            })
+        ]);
+    return res.json(results.map(async r => await r.json()));
 }
