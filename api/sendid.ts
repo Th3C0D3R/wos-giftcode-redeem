4f3757030d39b777bfe97ac333aaff060b5f5f04
TH3C0D3R <7798500+Th3C0D3R@users.noreply.github.com>
2024-03-27 10:25:21 +0100

    update test

--- api/sendid.ts
+++ api/sendid.ts
@@ -1,14 +1,13 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import fs from "fs";
-import path from "path";
+import { sql } from "@vercel/postgres";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     var data = req.query;
     if (data["id"] === undefined) return res.json({ status: 500, data: "NO PLAYER ID" });
 
-    var idFile: string = path.join(process.cwd(), "data", 'ids.json');
-    var ids: Array<number> = JSON.parse(fs.readFileSync(idFile, { encoding: "utf-8" })) ?? [];
-
+    var ids = await sql`SELECT * FROM ids`;
+    console.log(ids)
+    return res.json({ status: 200, data: "IDs added" });
     ids["ids"] = ids["ids"].concat([data["id"]]);
     ids["ids"].sort(function (a: number, b: number) {
         return a - b;
@@ -16,7 +15,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
     ids["ids"] = [...new Set(ids["ids"])];
 
-    fs.writeFileSync(idFile, JSON.stringify(ids, null, 4));
 
     return res.json({ status: 200, data: "IDs added" });
 }
