9a02a96a378b946b4a806299201e1ea797a10d42
TH3C0D3R <7798500+Th3C0D3R@users.noreply.github.com>
2024-05-07 18:06:15 +0200

    update

--- api/getdl.ts
+++ api/getdl.ts
@@ -2,16 +2,24 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     return res.json({ message: "WIP" });
+    const getHighestQualityURL = (arrQ, arrU) => {
+        return arrU[arrQ.indexOf(Math.max(...arrQ).toString())];
+    };
     var data = req.query;
+    var isP = false;
     if (data["url"] === undefined) return res.json({ message: "NO URL" });
+    if (data["p"] !== undefined) isP = true;
+    const urlBase = isP ? "https://yesdownloader.com/" : "https://www.downloader.wiki/";
+    const convertRoute = isP ? "en1/convert/" : "convert/";
+    const ref = isP ? `${urlBase}en1/` : `${urlBase}?err=dmFyOmVycl9lbnRlcl91cmw`;
+    const durl = isP ? `url=${encodeURI(data["url"].toString())}&submitForm=` : `from=all&url=${encodeURI(data["url"].toString())}`;
+    const regExU = isP ? /href\="\/(en1\/sdownload\/\S+)"/ : /href\="\/(sdownload\/\S+)"/
 
-    var qualityUrl = {};
-
-    var resConvert = await fetch("https://yesdownloader.com/en1/convert/", {
+    var resConvert = await fetch(`${urlBase}${convertRoute}`, {
         "credentials": "include",
         "headers": {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/jxl,image/webp,*/*;q=0.8",
             "Accept-Language": "en-US,en;q=0.5",
             "Content-Type": "application/x-www-form-urlencoded",
             "Sec-GPC": "1",
@@ -19,23 +27,52 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
             "Sec-Fetch-Dest": "document",
             "Sec-Fetch-Mode": "navigate",
             "Sec-Fetch-Site": "same-origin",
-            "Sec-Fetch-User": "?1",
-            "Pragma": "no-cache",
-            "Cache-Control": "no-cache"
+            "Sec-Fetch-User": "?1"
         },
-        "referrer": "https://yesdownloader.com/en1/",
-        "body": "url={url decoded}&submitForm=",
+        "referrer": `${ref}`,
+        "body": durl,
         "method": "POST",
         "mode": "cors"
     });
-
+    
     var resText = await resConvert.text();
-    //<td>(\d{3,4})<td>[\s\S]+href\=(\/en1\/sdownload\S+)
 
-    var regQ = [...resText.matchAll(new RegExp(/<td>(\d{3,4})<td>/,"gi"))];
-    var regU = [...resText.matchAll(new RegExp(/href\=(\/en1\/sdownload\S+)/,"gi"))];
+    const regQ = [...resText.matchAll(new RegExp(/<td>(\d{3,4})<\/td>/, "gi"))].map(a => a["1"]);
+    const regU = [...resText.matchAll(new RegExp(regExU, "gi"))].map(a => a["1"]);
 
+    var urlOfHighest = getHighestQualityURL(regQ, regU);
 
+    var resDL = await fetch(`https://yesdownloader.com/en1/sdownload/qcnAaMraZk9LRbane_ZJZ9iGaronOwn4FnmftUBj_MI/`, {
+        "credentials": "include",
+        "headers": {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/jxl,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Sec-GPC": "1",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-User": "?1"
+        },
+        "method": "GET",
+        "mode": "cors"
+    });
+    
+
+    console.log(resDL.redirected);
+
+    var dlText = await resDL.text();
+
+    const regS = [...dlText.matchAll(new RegExp(/<script>[\n](.+)<\/script>/, "g"))];
+    var script = regS[0][1];
+
+    var indexofEnd = script.indexOf("function mJHlA()");
+    var rest = script.substring(0, indexofEnd);
+
+    var x = eval(rest);
+
+    console.log(x);
 
     return res.json({});
 }
