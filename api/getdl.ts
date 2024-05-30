e134700ed19343c85042e816cb0ef83f0b0c6666
TH3C0D3R <7798500+Th3C0D3R@users.noreply.github.com>
2024-05-30 16:23:02 +0200

    update 2

--- api/getdl.ts
+++ api/getdl.ts
@@ -16,7 +16,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     const urlBase = isP ? "https://yesdownloader.com/" : "https://www.downloader.wiki/";
 
     var path = await chromium.executablePath("https://github.com/Sparticuz/chromium/releases/download/v123.0.1/chromium-v123.0.1-pack.tar");
-    chromium.setHeadlessMode = "shell";
     chromium.setGraphicsMode = false;
 
     let browser = await puppeteer.launch({
