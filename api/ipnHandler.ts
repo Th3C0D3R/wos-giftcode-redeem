3e0705ed5bca48e231466fb00f0f485027f8004f
TH3C0D3R <7798500+Th3C0D3R@users.noreply.github.com>
2024-05-31 08:46:55 +0200

    update test

--- api/ipnHandler.ts
+++ api/ipnHandler.ts
@@ -15,41 +15,41 @@ function getPaypalURI() {
 
 export default async function POST(req: VercelRequest, res: VercelResponse) {
     console.log('IPN Notification Event Received');
-    console.log(req.body);
 
     if (req.method !== 'POST') {
         console.error('Request method not allowed.');
         return res.status(405).send('Method Not Allowed');
     } else {
         console.log('IPN Notification Event received successfully.');
+
+        const ipnTransactionMessage = req.body;
+
+        // Build the body of the verification post message by prefixing 'cmd=_notify-validate'.
+        const verificationBody = `cmd=_notify-validate&${req.body}`;
+
+        console.log(`Verifying IPN: ${verificationBody}`);
+
+        const verifyResponse = await axios({
+            method: 'post',
+            url: getPaypalURI(),
+            data: verificationBody,
+        });
+        if (verifyResponse.status !== 200) {
+
+            console.log(`Invalid IPN: IPN message for ID: ${ipnTransactionMessage.txn_id} failed with code ${verifyResponse.status}`);
+            return res.json({ message: `Invalid IPN: IPN message for ID: ${ipnTransactionMessage.txn_id} failed with code ${verifyResponse.status}` });
+        }
+
+        if (verifyResponse.data !== 'VERIFIED') {
+
+            console.error(`Invalid IPN: Message for ID: ${ipnTransactionMessage.txn_id} is invalid (${verifyResponse.data}).`);
+            return res.json({ message: `Invalid IPN: Message for ID: ${ipnTransactionMessage.txn_id} is invalid (${verifyResponse.data}).` });
+        }
+
+        console.log(`Verified IPN: IPN message for ID: ${ipnTransactionMessage.txn_id} was verified.`);
+        console.log(JSON.stringify(ipnTransactionMessage));
+
+
         res.status(200).end();
     }
-
-    const ipnTransactionMessage = req.body;
-
-    // Build the body of the verification post message by prefixing 'cmd=_notify-validate'.
-    const verificationBody = `cmd=_notify-validate&${req.body}`;
-
-    console.log(`Verifying IPN: ${verificationBody}`);
-
-    const verifyResponse = await axios({
-        method: 'post',
-        url: getPaypalURI(),
-        data: verificationBody,
-    });
-    if (verifyResponse.status !== 200) {
-
-        console.log(`Invalid IPN: IPN message for ID: ${ipnTransactionMessage.txn_id} failed with code ${verifyResponse.status}`);
-        return res.json({ message: `Invalid IPN: IPN message for ID: ${ipnTransactionMessage.txn_id} failed with code ${verifyResponse.status}` });
-    }
-
-    if (verifyResponse.data !== 'VERIFIED') {
-
-        console.error(`Invalid IPN: Message for ID: ${ipnTransactionMessage.txn_id} is invalid (${verifyResponse.data}).`);
-        return res.json({ message: `Invalid IPN: Message for ID: ${ipnTransactionMessage.txn_id} is invalid (${verifyResponse.data}).` });
-    }
-
-    console.log(`Verified IPN: IPN message for ID: ${ipnTransactionMessage.txn_id} was verified.`);
-    console.log(JSON.stringify(ipnTransactionMessage));
-
 }
