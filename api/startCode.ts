c739d0ee102d546685ed3c7d021ae21de2313dab
TH3C0D3R <7798500+Th3C0D3R@users.noreply.github.com>
2024-06-05 22:48:08 +0200

    update

--- api/startCode.ts
+++ api/startCode.ts
@@ -2,27 +2,22 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default async function handler(req: VercelRequest, res: VercelResponse){
     var data = req.query;
-    console.log(data);
     if (data["code"] === undefined) return res.json({ message: "NO CODE" });
     if (data["app"] === undefined) return res.json({ message: "NO application_id" });
     if (data["token"] === undefined) return res.json({ message: "NO token" });
-    console.log(0);
     const giftcode = data["code"] ?? "";
     const appID = data["app"] ?? "";
     const token = data["token"] ?? "";
-    console.log(1);
     var result = await fetch(`https://wgr.vercel.app/api/doList?code=${giftcode}`);
-    console.log(2);
     var redeemResult = await result.text();
-    console.log(3);
-    await fetch(`https://discord.com/api/v10/webhooks/${appID}/${token}/messages/@original`, {
+    fetch(`https://discord.com/api/v10/webhooks/${appID}/${token}/messages/@original`, {
         method: 'PATCH',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            content: redeemResult
+            content: redeemResult.replace("<br>","\n")
         })
     });
-    console.log(4);
+    return res.status(200).end();
 }
