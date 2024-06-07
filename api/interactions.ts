ac17e4cc4f580124e20ea0219509e74cd46e3bac
TH3C0D3R <7798500+Th3C0D3R@users.noreply.github.com>
2024-06-07 09:42:53 +0200

    test

--- api/interactions.ts
+++ api/interactions.ts
@@ -37,7 +37,6 @@ const handleResponse = async (body: any) => {
                     return returnInteraction(`You have no permission to execute the interaction!`);
                 }
                 var giftcode = data?.options[0]?.value as string ?? "";
-                //console.log(`\nhttps://wgr.vercel.app/api/startCode?code=${giftcode}&app=${application_id}&token=${token}\n`);
                 fetch(`https://wgr.vercel.app/api/startCode?code=${giftcode}&app=${application_id}&token=${token}`);
                 await sleep(2500);
                 return returnAckn(`The process has started...\nA message will send as soon as the process has finished`);
--- api/startCode.ts
+++ api/startCode.ts
@@ -2,15 +2,19 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default async function handler(req: VercelRequest, res: VercelResponse){
     var data = req.query;
+    console.log(1);
     if (data["code"] === undefined) return res.json({ message: "NO CODE" });
     if (data["app"] === undefined) return res.json({ message: "NO application_id" });
     if (data["token"] === undefined) return res.json({ message: "NO token" });
+    console.log(2);
     const giftcode = data["code"] ?? "";
     const appID = data["app"] ?? "";
     const token = data["token"] ?? "";
     var result = await fetch(`https://wgr.vercel.app/api/doList?code=${giftcode}`);
+    console.log(3);
     var redeemResult = await result.text();
-    fetch(`https://discord.com/api/v10/webhooks/${appID}/${token}/messages/@original`, {
+    console.log(4);
+    await fetch(`https://discord.com/api/v10/webhooks/${appID}/${token}/messages/@original`, {
         method: 'PATCH',
         headers: {
             'Content-Type': 'application/json'
@@ -19,5 +23,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse){
             content: redeemResult.replace("<br>","\n")
         })
     });
+    console.log(5);
     return res.status(200).end();
 }
